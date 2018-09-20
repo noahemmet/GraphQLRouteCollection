e59603613972653d0410440f49260cc1563650bd
Noah Emmet <noah@noahemmet.com>
2018-09-20 16:59:37 -0700

    removed unused code

--- Sources/VaporGraphQL/resolveWithFuture.swift
+++ Sources/VaporGraphQL/resolveWithFuture.swift
@@ -5,24 +5,24 @@
 import GraphQL
 import Vapor
 
-public func resolveWithFuture<Source, Result>(resolver: @escaping TypedFieldResolver1<Source, Future<Result?>>) throws -> GraphQLFieldResolve {
-  return withTypedResolve { (source: Source, args: Map) -> Result? in
-    try resolver(source, args)?.wait()
-  }
-}
-
-public func resolveWithFuture<Source, Context, Result>(
-  resolver: @escaping TypedFieldResolver2<Source, Context, Future<Result?>>
-) throws -> GraphQLFieldResolve {
-  return withTypedResolve { (source: Source, args: Map, context: Context) -> Result? in
-    try resolver(source, args, context)?.wait()
-  }
-}
-
-public func resolveWithFuture<Source, Context, Result>(
-  resolver: @escaping TypedFieldResolver3<Source, Context, Future<Result?>>
-) throws -> GraphQLFieldResolve {
-  return withTypedResolve { (source: Source, args: Map, context: Context, info: GraphQLResolveInfo) -> Result? in
-    try resolver(source, args, context, info)?.wait()
-  }
-}
+//public func resolveWithFuture<Source, Result>(resolver: @escaping TypedFieldResolver1<Source, Future<Result?>>) throws -> GraphQLFieldResolve {
+//  return withTypedResolve { (source: Source, args: Map) -> Result? in
+//    try resolver(source, args)?.wait()
+//  }
+//}
+//
+//public func resolveWithFuture<Source, Context, Result>(
+//  resolver: @escaping TypedFieldResolver2<Source, Context, Future<Result?>>
+//) throws -> GraphQLFieldResolve {
+//  return withTypedResolve { (source: Source, args: Map, context: Context) -> Result? in
+//    try resolver(source, args, context)?.wait()
+//  }
+//}
+//
+//public func resolveWithFuture<Source, Context, Result>(
+//  resolver: @escaping TypedFieldResolver3<Source, Context, Future<Result?>>
+//) throws -> GraphQLFieldResolve {
+//  return withTypedResolve { (source: Source, args: Map, context: Context, info: GraphQLResolveInfo) -> Result? in
+//    try resolver(source, args, context, info)?.wait()
+//  }
+//}
